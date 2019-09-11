9bcbad8e5cd9747d6f40eb4527009d27a25aa55d
Alexandru <1010373+alexandruc@users.noreply.github.com>
2019-09-11 17:55:54 +0300

    Add alternative html parsing with html.parser

--- content_extraction_utils.py
+++ content_extraction_utils.py
@@ -1,9 +1,23 @@
+from enum import Enum, unique
+from html.parser import HTMLParser
+
+@unique
+class ParsingType(Enum):
+    MANUAL = 1
+    HTMLPARSER = 2
 
 class ContentExtractionUtils:
     '''Some utility methods for parsing scraped content'''
     
-    def extractLinks(self, content : str) -> list:
+    def extractLinks(self, content : str, method = ParsingType.HTMLPARSER) -> list:
         ''' Extracts links from a content parameter '''
+        if method == ParsingType.MANUAL:
+           return self.__extractLinksManual(content)
+        elif method == ParsingType.HTMLPARSER:
+            return self.__extractLinksHtmlParser(content)
+        return []
+
+    def __extractLinksManual(self, content):
         start = 0
         links = []
         linkStartPattern = "href=\""
@@ -14,12 +28,28 @@ class ContentExtractionUtils:
                 end = content.find("\"", start+len(linkStartPattern))
                 if end != -1:
                     data = content[start : end]
-                    if self.__validateLink(data):
+                    if ContentExtractionUtils.validateLink(data):
                         links.append(data)
                     start = end+1
         return links
 
-    def __validateLink(self, link : str):
+    def __extractLinksHtmlParser(self, content):
+        html_parser = LinkExtractor()
+        html_parser.feed(content)
+        return html_parser.get_results()
+
+    @staticmethod
+    def validateLink(link : str):
         ''' poor man's check if link is valid http link '''
         return link.startswith("http")
 
+class LinkExtractor(HTMLParser):
+    __links = []
+
+    def handle_starttag(self, tag, attrs):
+        for name, value in attrs:
+            if name == "href" and ContentExtractionUtils.validateLink(value) :
+                self.__links.append(value)
+
+    def get_results(self):
+        return self.__links
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from simple_crawler_requests import SimpleCrawlerRequests
 from simple_crawler_http import SimpleCrawlerHttp
-from content_extraction_utils import ContentExtractionUtils
+from content_extraction_utils import ContentExtractionUtils, ParsingType
 
 if __name__ == "__main__":
     crawlerReq = SimpleCrawlerRequests()
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     output.write(data)
     #get all links in content
     tools = ContentExtractionUtils()
-    links = tools.extractLinks(data)
-    print(links)
+    links = tools.extractLinks(data, ParsingType.MANUAL)
+    print(len(links))
+    links = tools.extractLinks(data, ParsingType.HTMLPARSER)
+    print(len(links))
     
     print("All done!")
     
